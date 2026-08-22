7a4ba02dc63e03cb174e40900ac8237b6757ea31
JoyinJoester <lichaoran8@gmail.com>
2026-08-22 21:30:24 +0800

    fix(webdav): import compose modifier

--- Monica for Android/app/src/main/java/takagi/ru/monica/ui/screens/WebDavCertificateDialog.kt
+++ Monica for Android/app/src/main/java/takagi/ru/monica/ui/screens/WebDavCertificateDialog.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import takagi.ru.monica.webdav.WebDavCertificateTrustStore
 import takagi.ru.monica.webdav.WebDavUntrustedCertificateException
